47ee2eedb5556c17e2e4270cfdab84203d671d76
Theophile Avoyne <tavoyne@gmail.com>
2022-03-16 17:46:16 +0100

    Set up eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,26 @@
+require("@rushstack/eslint-patch/modern-module-resolution");
+
 module.exports = {
-  extends: "next/core-web-vitals",
+  extends: ["@steadier"],
+  ignorePatterns: [
+    "!.*",
+    "**/*.d.ts",
+    "/.next/**/*",
+    "/.pnp.cjs",
+    "/.pnp.loader.mjs",
+    "/.yarn/**/*",
+  ],
+  root: true,
+  rules: {
+    "func-style": "off",
+    "no-duplicate-imports": "off",
+    "no-underscore-dangle": "off",
+    "no-unused-vars": "off",
+    "node/no-missing-import": "off",
+    "node/no-unpublished-import": "off",
+    "node/no-unpublished-require": "off",
+    "node/no-unsupported-features/es-syntax": "off",
+    "prefer-arrow-callback": "off",
+    strict: "off",
+  },
 };
